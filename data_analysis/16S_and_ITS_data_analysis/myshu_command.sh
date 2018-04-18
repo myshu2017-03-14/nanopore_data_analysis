368eac5d2e77784eb6dc7dfb851d2266e92d261e
Myshu <1291016966@qq.com>
2018-04-18 11:23:27 +0800

    update

--- data_analysis/16S_and_ITS_data_analysis/myshu_command.sh
+++
@@ -1,9 +0,0 @@
-
-# run blast
-nohup ./blastn.sh /analysis/software_han/3-finaldata/nanopore-data-out/20180318_add_7_samples_and_1_samples/all_data_out/data/ /analysis/software_han/3-finaldata/nanopore-data-out/20180318_add_7_samples_and_1_samples/all_data_out/data/ &
-
-# count each level abundance 
-./count_taxa_abundance.sh
-
-# cat all samples taxa abundance of each level
-./cat_taxa_abundance.sh
