e39a16b0adc702603a3ca32fb9bedaf3d5a0bd84
Myshu <1291016966@qq.com>
2018-04-18 10:23:41 +0800

    split barcode

---
+++ 1-split_barcode/porechop_test.sh
@@ -0,0 +1,12 @@
+in=$1  # FAH09535_test_all_reads.fastq
+# test1
+#porechop-runner.py -i fastq_runid_4a7087c5b17141c19008f2f2ace24dca27462351.fastq -b porechop_output_test1 --format fastq -t 5 
+
+# test2
+#porechop-runner.py -i $in -b porechop_output_85_two_barcodes --format fastq -t 5 --barcode_threshold 85 --require_two_barcodes
+
+porechop-runner.py -i $in -b porechop_output_85 --format fastq -t 5 --barcode_threshold 85
+
+# test3
+#porechop-runner.py -i fastq_runid_4a7087c5b17141c19008f2f2ace24dca27462351.fastq -b porechop_output_test3 --format fastq -t 5 --barcode_threshold 60 --barcode_diff 1
+
