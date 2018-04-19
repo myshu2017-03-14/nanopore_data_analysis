9f31c74ba7ca5f18f2adf0599d18596fe7ce0695
Myshu <1291016966@qq.com>
2018-04-19 09:07:06 +0800

    update

--- 1-split_barcode/porechop_test.sh
+++ 1-split_barcode/porechop_test.sh
@@ -1,3 +1,4 @@
+date
 in=$1  # FAH09535_test_all_reads.fastq
 # test1
 #porechop-runner.py -i fastq_runid_4a7087c5b17141c19008f2f2ace24dca27462351.fastq -b porechop_output_test1 --format fastq -t 5 
@@ -10,3 +11,4 @@ porechop-runner.py -i $in -b porechop_output_85 --format fastq -t 5 --barcode_th
 # test3
 #porechop-runner.py -i fastq_runid_4a7087c5b17141c19008f2f2ace24dca27462351.fastq -b porechop_output_test3 --format fastq -t 5 --barcode_threshold 60 --barcode_diff 1
 
+date
